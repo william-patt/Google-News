7b81d7108ea1ede9892deaa4596f6c7fe49c12e6
william-patt <43737774+william-patt@users.noreply.github.com>
2020-08-09 22:02:45 +1200

    Update Scraper Script.R

--- TBS-NEWS-Article-Scraper/Scraper Script.R
+++ TBS-NEWS-Article-Scraper/Scraper Script.R
@@ -8,26 +8,16 @@ library(readr)
 
 
 #Gets news article
-#news <- xml2::read_html(readline(prompt = "Enter URL (tbs):"))
-#news <- xml2::read_html("https://news.tbs.co.jp/newseye/tbs_newseye3849504.html")
+news <- xml2::read_html(readline(prompt = "Enter URL (tbs):"))
 
 #test case "https://news.tbs.co.jp/newseye/tbs_newseye3750343.html"
-#konbini <- read_csv("C:/Users/William/Desktop/texts/konbini.txt", 
-                    #col_names = FALSE)
-konbini <- read_csv("C:/Users/William/Desktop/new 1.txt", 
-                  col_names = FALSE)
-View(new_1)
-#data <- news %>%
-  #html_nodes(".md-mainArticle p") %>%
-  #html_text()
 
-test1 <- as.vector(t(konbini))
-View(test1)
+data <- news %>%
+  html_nodes(".md-mainArticle p") %>%
+  html_text()
 
 #Splits sentence at each full stops, paragraphs, etc
-news_data <- unlist(tokenize_sentences(test1))
-
-View(news_data)
+news_data <- unlist(tokenize_sentences(data))
 
 #Remove duplicated sentences
 li1 <- lapply(news_data, function(x) unique(x))
@@ -35,8 +25,6 @@ i1 <- !(duplicated(li1)|duplicated(li1, fromLast = TRUE))
 news_data <- news_data[i1]
 
 dat <- as.data.frame(news_data)
-
-
 
 #lapply(news_data, write, "Sentences.txt", append=TRUE)
 write_csv(dat, "Sentences.txt", append=TRUE)
